e18e7ab5619fd2891b1458bbe34a6dcf549f565f
rasy-js <rasy.js@gmail.com>
2018-05-16 18:06:43 +0300

    Upd

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var server = require('http');
+var express = require('express'),
+    fs = require('fs'),
+    app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var app = express();
 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port );
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+app.get('/', function(req, res) {
+    res.send('Hello from NodeJS  at '+ new Date());
 });
+
+
+app.listen(8080, ip);
+
+module.exports = app;
